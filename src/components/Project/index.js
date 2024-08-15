40ee8d1d173fa0dede914927c7f02d835da3cead
ksa205 <karanbir_singh@sfu.ca>
2024-08-14 17:47:32 -0700

    added updates to the projects

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -5,6 +5,8 @@ import { useEffect, useState } from 'react'
 import IMG1 from '../../assets/images/imageproject.jpeg'
 import IMG2 from '../../assets/images/imageproject-2.jpeg'
 import IMG3 from '../../assets/images/TicTacToe.jpeg'
+import IMG4 from '../../assets/images/proxyServer2.jpeg'
+import IMG5 from '../../assets/images/dataTransfer.jpeg'
 
 const Project = () => {
     const[letterClass, setLetterClass] = useState('text-animate')
@@ -15,7 +17,7 @@ const Project = () => {
             3000
           );
       }, [])
-
+      
     return (
         <>
             <div className='container project-page'>
@@ -29,6 +31,24 @@ const Project = () => {
                     </h1>
                 </div>
                 <div className='divi'>
+                        <article className='portfolio-item'>
+                            <div className='portfolio-item-image'>
+                                <img src={IMG4} alt=''/>
+                            </div>
+                            <h3>Web & Web Proxy</h3>
+                            <div className='portfolio-item-cta'>
+                                <a href='https://github.com/karanjhand/Web-Server' className='btn' target='blank'>Github</a>
+                            </div>
+                        </article>
+                        <article className='portfolio-item'>
+                            <div className='portfolio-item-image'>
+                                <img src={IMG5} alt=''/>
+                            </div>
+                            <h3>Pipelined Reliable Transfer Protocol</h3>
+                            <div className='portfolio-item-cta'>
+                                <a href='https://github.com/karanjhand/Pipelined-Reliable-Transfer-Protocol' className='btn' target='blank'>Github</a>
+                            </div>
+                        </article>
                         <article className='portfolio-item'>
                             <div className='portfolio-item-image'>
                                 <img src={IMG3} alt=''/>
@@ -59,7 +79,7 @@ const Project = () => {
                     </div>
                 
                 </div>
-            
+                
                 
             <Loader type='pacman'/>
         </>
